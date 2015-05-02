f4ee8460a058108e64d7f7343d5361f0172d0993
Planeshifter <pburckhardt@outlook.com>
2015-05-01 23:28:14 -0400

    [UPDATE] use blas1

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 'use strict';
 
+var blas1 = require('ndarray-blas-level1');
+
 module.exports =  function cholesky(A, L) {
 
 	if ( A.dimension !== 2 ) {
 		return false;
-	} 
-
-    var n = A.shape[0];
-
-    for ( var i = 0; i < n; i++ ) {
-        for ( var j = 0; j < (i+1); j++ ) {
-            var s = 0;
-            for ( var k = 0; k < j; k++ ) {
-                s += L.get(i, k) * L.get(j, k);
-            }
-            var res = (i === j) ?
-                           Math.sqrt(A.get(i, i) - s) :
-                           (1 / L.get(j, j) * (A.get(i, j) - s));
-            L.set(i, j, res);
-        }
 	}
 
-    return true;
+	var n = A.shape[0];
+
+	for ( var i = 0; i < n; i++ ) {
+    	for ( var j = 0; j < (i+1); j++ ) {
+
+			var s = blas1.dot(L.pick(i,null).hi(j), L.pick(j,null).hi(j));
+			var res = (i === j) ?
+				Math.sqrt(A.get(i, i) - s) :
+				(1 / L.get(j, j) * (A.get(i, j) - s));
+			
+			L.set(i, j, res);
+		}
+	}
+
+	return true;
 };
